f16bae614870c9c2be514b1021fd8143aba8df20
Rafael Fernández <github@rafaelfernandez.dev>
2024-12-25 18:06:02 +0100

    folder src in multimodule

---
+++ src/main/g8/$if(multiModule.truthy)$m1$endif$/src/main/scala/$package__packaged$/WordCount.scala
@@ -0,0 +1,29 @@
+package $organization$.$name$
+
+/**
+ * Everyone's favourite wordcount example.
+ */
+
+import org.apache.spark.rdd._
+
+object WordCount {
+
+  /**
+   * A slightly more complex than normal wordcount example with optional
+   * separators and stopWords. Splits on the provided separators, removes
+   * the stopwords, and converts everything to lower case.
+   */
+  def withStopWordsFiltered(
+      rdd: RDD[String],
+      separators: Array[Char] = " ".toCharArray,
+      stopWords: Set[String] = Set("the")
+  ): RDD[(String, Int)] = {
+
+    val tokens: RDD[String]      = rdd.flatMap(_.split(separators).map(_.trim.toLowerCase))
+    val lcStopWords: Set[String] = stopWords.map(_.trim.toLowerCase)
+    val words: RDD[String] = tokens.filter(token => !lcStopWords.contains(token) && token.nonEmpty)
+    val wordPairs: RDD[(String, Int)]  = words.map((_, 1))
+    val wordCounts: RDD[(String, Int)] = wordPairs.reduceByKey(_ + _)
+    wordCounts
+  }
+}
