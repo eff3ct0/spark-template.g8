f4d47f758450e95cb9bd71e30a441ae6ca1f7b0e
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2024-12-25 17:08:16 +0000

    Src folder in multimodule (#1)

---
+++ src/main/g8/$if(multiModule.truthy)$m1$endif$/src/test/scala/$package__packaged$/WordCountTest.scala
@@ -0,0 +1,35 @@
+package $organization$.$name$
+
+/**
+ * A simple test for everyone's favourite wordcount example.
+ */
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.apache.spark.rdd.RDD
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class WordCountTest extends AnyFlatSpecLike with Matchers with SharedSparkContext {
+
+  "WordCount" should "count words with Stop Words Removed" in {
+    val linesRDD: RDD[String] = sc.parallelize(
+      Seq(
+        "How happy was the panda? You ask.",
+        "Panda is the most happy panda in all the#!?ing land!"
+      )
+    )
+
+    val stopWords: Set[String]   = Set("a", "the", "in", "was", "there", "she", "he")
+    val splitTokens: Array[Char] = "#%?!. ".toCharArray
+
+    val wordCounts: RDD[(String, Int)] =
+      WordCount.withStopWordsFiltered(linesRDD, splitTokens, stopWords)
+    val wordCountsAsMap: collection.Map[String, Int] = wordCounts.collectAsMap()
+
+    assert(!wordCountsAsMap.contains("the"))
+    assert(!wordCountsAsMap.contains("?"))
+    assert(!wordCountsAsMap.contains("#!?ing"))
+    assert(wordCountsAsMap.contains("ing"))
+    wordCountsAsMap("panda") should be(3)
+  }
+}
